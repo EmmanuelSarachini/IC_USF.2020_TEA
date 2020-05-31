5f12a23bf6d925ba87efc1c609621739226bff42
Emmanuel Sarachini <eoliveira95@gmail.com>
2020-05-30 22:21:12 -0300

    Teste

---
+++ Sandbox/helloWord.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="app">
+        {{ message }}
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+let app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Primeiro teste em Vue!'
+    }
+})
+</script>
+
+<style lang="stylus">
+
+</style>
